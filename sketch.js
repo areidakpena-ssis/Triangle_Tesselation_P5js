ab7e8703ba3599a57e2e2982def281effa88dcac
areidakpena-ssis <153045302+areidakpena-ssis@users.noreply.github.com>
2024-01-11 09:00:57 +0700

    Create sketch.js
    File contains starter code for assignment.

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  // intialize canvas
+  CANVAS_WIDTH = 600
+  CANVAS_HEIGHT = 400
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+  
+  // set triangle dimensions
+  TRIANGLE_WIDTH = CANVAS_WIDTH / 3
+  TRIANGLE_HEIGHT = CANVAS_HEIGHT / 5
+  
+  
+}
+
+function draw() {
+  background(220)
+  // code for triangle tesselation goes here
+}
